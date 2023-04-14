121fa719baf488eb3b7743ba9a616944de8396aa
lauriehoude <2052728@cmontmorency.qc.ca>
2023-04-14 14:41:19 -0400

    dgjt

--- script/script.js
+++ script/script.js
@@ -1,2 +1,16 @@
+let myMinuteur;
 let anim = gsap
 .to(".fleche", { x: "0vmin", y:"6vmin", ease: "none", repeat:-1 });
+let sprite=document.querySelector(".sprite")
+
+let timerEnd= clearTimeout(function(){
+    document.body.classList.remove('.is-scrolling');
+})
+myMinuteur = setTimeout(timeEnd,10000);
+document.body.classList.add('.is-scrolling');
+
+window.addEventListener('scroll',(myMinuteur) => {
+    document.body.classList.add('.is-scrolling');
+  
+   
+});
