105eec336cbc9cc205e984b09a3a0b385bfcc22e
lauriehoude <2052728@cmontmorency.qc.ca>
2023-04-17 14:10:25 -0400

    ik

--- script/script.js
+++ script/script.js
@@ -2,15 +2,16 @@ let myMinuteur;
 let anim = gsap
 .to(".fleche", { x: "0vmin", y:"6vmin", ease: "none", repeat:-1 });
 let sprite=document.querySelector(".sprite")
+let body = document.querySelector("body")
 
-let timerEnd= clearTimeout(function(){
-    document.body.classList.remove('.is-scrolling');
-})
-myMinuteur = setTimeout(timeEnd,10000);
-document.body.classList.add('.is-scrolling');
+
+document.body.classList.remove('is-scrolling');
 
 window.addEventListener('scroll',(myMinuteur) => {
-    document.body.classList.add('.is-scrolling');
-  
+    clearTimeout(myMinuteur);
+    body.classList.add('is-scrolling');
+    myMinuteur = setTimeout(function(){body.classList.remove('is-scrolling');
+},1000);
    
 });
+
