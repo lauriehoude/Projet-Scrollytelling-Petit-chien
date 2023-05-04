2e868f7c63f148d4200eace1bd73c4b3a0929657
lauriehoude <2052728@cmontmorency.qc.ca>
2023-05-03 21:28:31 -0400

    tdhrs

--- script/script.js
+++ script/script.js
@@ -98,37 +98,97 @@ let chapitre1 = gsap.timeline({
 scrollTrigger: {
     scrub: true,
     markers: true,
-    start: '75% 0%',
+    start: '0% 70%',
     end: 'center top',
-    trigger: '.chapitre1',
+    trigger: '#chapitre1',
 }
 });
 
-chapitre1.to(".personnage-fatigue", { y: `-30vh`, x: `20vh`, duration: 3,  });
-chapitre1.to(".personnage-fatigue", { x: `100vw`, duration: 4,  });
-chapitre1.to(".tombeau", { opacity: "0%", ease: true, duration: 5,  }, ">");
+chapitre1.to(".personnage-fatigue", { y: `-30vw`, x: `20vh`, duration: 3,  });
+chapitre1.to(".personnage-fatigue", {y: `30vw`, x: `80vw`, delay:4,  });
+chapitre1.to(".tombeau", { opacity: "100%", ease: true, duration: 5,  }, ">");
+chapitre1.to(".tombeau", { opacity: "50%", ease: true, duration: 5,  }, ">");
 
-let chapitre2 = gsap.timeline();
-chapitre2.to(".chien-courone", { y: "50vh", duration: 2, delay: 3, repeat: -1 });
-chapitre2.to(".courone", { x: "30vw", duration: 3, rotate: "360", repeat: -1 }, "<")
 
-let chapitre3 = gsap.timeline();
-chapitre3.to(".personnage", { x: "20vw", duration: 2, opacity: 0, scale: 1, delay: 1, repeat: -1 }, )
-chapitre3.to(".personnage", { x: "100vw", opacity: 100, duration: 1, scale: 2, repeat: -1 })
 
-let chapitre4 = gsap.timeline();
-chapitre4.to(".chien-nage", { x: "10vw", duration: 2, scale: 1, delay: 8, repeat: -1 }, )
-chapitre4.to(".chien-nage", { x: "100vw", duration: 1, scale: 2 })
 
-let chapitre6 = gsap.timeline()
-chapitre6.to(".nuage", { x: "0vw", duration: 2, ease: true, repeat: -1 });
-chapitre6.to(".nuage", { x: "3vw", duration: 3, repeat: -1 }, "<")
-chapitre6.to(".nuage", { x: "0vw", duration: 3, repeat: -1 }, "<")
-chapitre6.to(".chien-paradis", { duration: 1, ease: true, rotate: "-5", repeat: -1 }, "<")
-chapitre6.to(".chien-paradis", { x: "10vh", duration: 2, ease: true, rotate: "10", repeat: -1 })
-chapitre6.to(".chien-paradis", { x: "30vw", duration: 1, ease: true, repeat: -1 })
-chapitre6.to(".chien-paradis", { y: "-15vh", duration: 2, ease: true, opacity: 100, repeat: -1 })
-chapitre6.to(".chien-paradis", { y: "-40vh", duration: 2, ease: true, opacity: 0, repeat: -1 })
+let chapitre2 = gsap.timeline({
+
+
+
+scrollTrigger: {
+    scrub: true,
+    markers: true,
+    start: '50% 0%',
+    end: 'center bottom',
+    trigger: '#chapitre2',
+}
+});
+chapitre2.to(".chien-courone", { y: "50vh", duration: 2, delay: 3,  });
+chapitre2.to(".courone", { x: "30vw", duration: 3, rotate: "360",  }, "<")
+
+let chapitre3 = gsap.timeline({
+
+
+scrollTrigger: {
+    scrub: true,
+    markers: true,
+    start: 'center 85%',
+    end: '25% 10%',
+    trigger: '#chapitre3',
+}
+});
+chapitre3.to(".personnage", { x: "20vw", duration: 2, opacity: 0, scale: 1, delay: 1,  }, )
+chapitre3.to(".personnage", { x: "100vw", opacity: 100, duration: 1, scale: 2, })
+
+let chapitre4 = gsap.timeline({
+
+scrollTrigger: {
+    scrub: true,
+    markers: true,
+    start: '75% 0%',
+    end: 'center top',
+    trigger: '#chapitre4',
+}
+});
+
+chapitre4.to(".chien-nage", { x: "10vw", duration: 7, scale: 1, }, )
+chapitre4.to(".chien-nage", { x: "100vw", duration: 7, scale: 2 })
+
+let chapitre5=gsap.timeline({
+scrollTrigger: {
+    scrub: true,
+    markers: true,
+    pin:true,
+    start: '75% 0%',
+    end: 'center top',
+    trigger: '#chapitre5',
+}
+});
+chapitre5.to(".roche01",{ y: "-10vh", duration: 2, ease: true,  })
+chapitre5.to(".roche01",{ x: "70vw", duration: 2, ease: true,  })
+
+let chapitre6 = gsap.timeline({
+scrollTrigger: {
+    scrub: true,
+    markers: true,
+    pin:true,
+    start: 'center 50%',
+    end: '25% 10%',
+    trigger: '#chapitre6',
+    toggleActions: 'restart complete reverse reset',
+}
+});
+chapitre6.to(".nuage", { x: "0vw", duration: 2, ease: true,  });
+chapitre6.to(".nuage", { x: "3vw", duration: 3,  }, "<")
+chapitre6.to(".nuage", { x: "0vw", duration: 3,  }, "<")
+chapitre6.to(".chien-paradis", { duration: 1, ease: true, rotate: "-5",  }, "<")
+chapitre6.to(".chien-paradis", { x: "10vh", duration: 2, ease: true, rotate: "10",  })
+chapitre6.to(".chien-paradis", { x: "30vw", duration: 1, ease: true,  })
+chapitre6.to(".chien-paradis", { y: "-15vh", duration: 2, ease: true, opacity: 100,  })
+chapitre6.to(".chien-paradis", { y: "-40vh", duration: 2, ease: true, opacity: 0, })
+chapitre6.to(".montagne", { y: "0vh", duration: 2, ease: true,scale:1,  })
+chapitre6.to(".montagne", { y: "20vh", duration: 2, ease: true,scale:0.8,  })
 
 
 
